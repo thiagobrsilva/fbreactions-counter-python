de1e2f770a8c108ccb8bd7e8377e41e62af26542
thiagobrsilva <msndo.thiago@hotmail.com>
2017-05-17 10:40:05 -0700

    getting reach amount

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from urllib.parse import unquote
 import requests
 import sys
 import os
@@ -22,18 +23,19 @@ else:
 
 
 # Requesting feed
-reqFeed = requests.get("https://graph.facebook.com/v2.9/" + pageid + "/feed?access_token=" + token)
+req_feed = requests.get("https://graph.facebook.com/v2.9/" + pageid + "/feed?limit=100&access_token=" + token)
 
-if (reqFeed.status_code==400):
+if (req_feed.status_code==400):
     print("Invalid token")
     sys.exit()
 
 # JSON format
-feedData = reqFeed.json()
+feed_data = req_feed.json()
+
 
 # Output file
 f = open(os.getcwd()+'/output.csv', 'w')
-f.write("message;created_time;reactions\n")
+f.write("message_title;message_link;created_time;reactions;total_reach\n")
 
 # Counters: ix_match -> posts that are between selected dates ix_total -> all posts found
 ix_match = 0
@@ -41,49 +43,89 @@ ix_total = 0
 
 while (True):
 
-    for item in feedData['data']:
+    for item in feed_data['data']:
 
         # Getting post date
         post_date = datetime.strptime(item["created_time"][:10], "%Y-%m-%d")
 
         ix_total = ix_total + 1
-        print("Post Total Counter:" + str(ix_total))
+        print("Total Counter:" + str(ix_total))
 
         # Checking date
         if ((post_date>=from_date) and (post_date<=to_date)):
-            postID = item["id"]
+            post_id = item["id"]
 
-            # Requesting info about the post
-            reqPost = requests.get("https://graph.facebook.com/v2.9/" + postID + "/reactions?access_token=" + token + "&debug=all&format=json&method=get&pretty=0&summary=total_count&suppress_http_code=1")
-            postData = reqPost.json()
-
-            try:
-                reactions_counter = postData["summary"]["total_count"]
-            except:
-                reactions_counter = 0
-
-            # Shared posts don't have message element
+            # unknown posts don't have message element, usually they are new photos or some profile update
             try:
                 post_msg = item["message"]
             except:
-                post_msg = "shared"
+                post_msg = "unknown"
 
-            f.write(post_msg + ";" + item["created_time"] + ";" + str(reactions_counter) + "\n")
+            # Requesting info about the post
+            req_post = requests.get("https://graph.facebook.com/v2.9/" + post_id + "/reactions?access_token=" + token + "&debug=all&format=json&method=get&pretty=0&summary=total_count&suppress_http_code=1")
+
+            if (req_post.status_code == 400):
+                print("Invalid token")
+                sys.exit()
+
+            post_data = req_post.json()
+
+            # Getting the summary of reactions
+            try:
+                reactions_counter = post_data["summary"]["total_count"]
+            except:
+                reactions_counter = 0
+
+            req_attach = requests.get("https://graph.facebook.com/v2.9/" + post_id + "/attachments?access_token=" + token)
+
+            if (req_attach.status_code == 400):
+                print("Invalid token")
+                sys.exit()
+
+
+            # Getting post link
+            attach_data = req_attach.json()
+            for item_attach in attach_data['data']:
+                try:
+                    attach_link = item_attach["target"]["url"]
+                except:
+                    attach_link = ""
+
+            # Getting the reach amount - Facebook has a constraint that only returns a value for pages with more than 30 people
+            req_reach = requests.get("https://graph.facebook.com/v2.9/" + post_id + "/insights/post_impressions_unique?access_token=" + token)
+            if (req_reach.status_code == 400):
+                print("Invalid token")
+                sys.exit()
+
+            Reach_data = req_reach.json()
+            for item_reach in Reach_data['data']:
+                for item_reach_counter in item_reach['values']:
+                    try:
+                        reach_number = item_reach_counter["value"]
+                    except:
+                        reach_number = "0"
+
+            # cleaning' data
+            post_msg=post_msg.replace('\n',' ')
+            attach_link = unquote(unquote(attach_link))
+            attach_link=attach_link.replace('https://l.facebook.com/l.php?u=','')
+
+            f.write(post_msg+ ";" + attach_link + ";" + item["created_time"] + ";" + str(reactions_counter) + ";" + str(reach_number) + "\n")
 
             ix_match = ix_match + 1
-            print("Post Match Counter:" + str(ix_match))
+            print("Match Counter:" + str(ix_match))
 
     # Paging
     try:
-        next_page = feedData["paging"]["next"]
+        next_page = feed_data["paging"]["next"]
     except:
         next_page = ""
 
     if (next_page==""):
         break
     else:
-        reqFeed = requests.get(next_page)
-        feedData = reqFeed.json()
+        req_feed = requests.get(next_page)
+        feed_data = req_feed.json()
         print("next page")
 
 
